d053419a29df72b5202c9ce98f6d82b7b0eae18c
unknown <equiliberalium89@gmail.com>
2024-01-20 21:43:59 -0500

    .

---
+++ week6/test/array.js
@@ -0,0 +1,35 @@
+class Value{
+    constructor(value){
+        this.value = value;
+    }
+}
+
+module.exports = class Array{
+    constructor(){
+        this.item = []
+    }
+    
+    newItem = (newValue) => {
+        // return true // able to call Function
+        this.item.push(newValue);
+        return this.item;
+    };
+
+    viewItems = () => {return this.item};
+
+    deleteItem = (delItem) => {
+        let index = this.item.indexOf(delItem);
+        let obliterated = this.item.splice(index, 1);
+        return this.item;
+    };
+
+    searchArray = (searchTerm) => {
+        let answer = this.item.includes(searchTerm);
+        if (answer == true){
+            return answer
+        } 
+        else {
+            return 'Item not found.'
+        }
+    };
+}
